a01565a189d5f15d5d16126d8e0c6be94cf9c403
mengjun <mengjun1990@gmail.com>
2013-01-24 23:38:35 -0800

    chong gou

--- server.c
+++ server.c
@@ -5,44 +5,73 @@
 #include <netinet/in.h>
 #include <string.h>
 
-int main(){
-    int server_sockfd, client_sockfd;
-    int ret,addr_len;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-    
-    //create
-    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_sockfd<0){
+#define PORT 8080
+#define MAX_CONNS 128
+
+int create(){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd<0){
         printf("socket field\n");
-        return -1;
+        exit(-1);
     }
-     
-    //bind
+    return sockfd;
+}
+
+void bind2sock(int server_sockfd,int port){
+    struct sockaddr_in server_addr;
+ 
     bzero(&server_addr, sizeof(struct sockaddr_in));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = 
         htonl(INADDR_ANY);//htonl 把32位值从主机字节序转换成网络字节序
-    server_addr.sin_port = htons(8080);
-    ret = bind(server_sockfd, (struct sockaddr*)(&server_addr), 
+    server_addr.sin_port = htons(port);
+    int ret = bind(server_sockfd, (struct sockaddr*)(&server_addr), 
         sizeof(struct sockaddr));//htons 把16位值从主机字节序转换成网络字节序
     if(ret<0){
         printf("bind field\n");
-        return -1;
+        exit(-1);
     }
-    
-    //listen
-    ret = listen(server_sockfd, 128);
+ 
+}
+
+void listening(int server_sockfd){
+    int ret = listen(server_sockfd, MAX_CONNS);
     if(ret<0){
         printf("listen field\n");
-        return -1;
+        exit(-1);
     }
+}
+
+int main(){
+    int server_sockfd, client_sockfd;
+    int ret,addr_len;
+    struct sockaddr_in client_addr;
+    char buffer[1024];
+    //create
+    server_sockfd = create();
+ 
+    //bind
+    bind2sock(server_sockfd, PORT); 
+    
+    //listen
+    listening(server_sockfd); 
 
     //accept loop
     while(1){
         addr_len = sizeof(struct sockaddr);
-        ret = accept(server_sockfd, (struct sockaddr*)(&client_addr), &addr_len);
-        printf("accept ok!\r\nServer start get connect from %s : %s\r\n",ntohl(client_addr.sin_addr.s_addr),ntohs(client_addr.sin_port));
+        client_sockfd = accept(server_sockfd, (struct sockaddr*)(&client_addr), &addr_len);
+        printf("accept ok!\r\nServer start get connect from %s : %d\r\n",inet_ntoa(client_addr.sin_addr),client_addr.sin_port);
+        int recv_bytes = recv(client_sockfd, buffer, sizeof(buffer), 0);
+        if(recv_bytes<0){
+            printf("recv field");
+            close(client_sockfd);
+        }
+        buffer[recv_bytes] = '\0';
+        printf("recv data %d\r\n%s", recv_bytes, buffer);
+        char* msg = "HTTP/1.1 200 OK\r\nContent-Type:text/html;charset=UTF-8\r\n\r\nhello world\r\n";
+        printf("msg length:%d\n",strlen(msg));
+        send(client_sockfd, msg, strlen(msg), 0);
+        close(client_sockfd);
     }
     return 0;
 }
