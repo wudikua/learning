bfade2de7e4da78ed95b33c60c82e60665e0658d
jun meng <mengjun1990@gmail.com>
2013-02-04 17:46:58 +0800

    print error

--- server.c
+++ server.c
@@ -52,7 +52,7 @@ void socket_recv(int sockfd){
     while(flag){
         recv_bytes = recv(sockfd, buffer, sizeof(buffer), 0);
         if(recv_bytes<=0){
-            printf("recv field");
+            printf("recv field\r\n");
             close(sockfd);
             return;
         }else{
@@ -72,17 +72,17 @@ void socket_recv(int sockfd){
 void socket_send(int sockfd, const char* msg){
     int ret = send(sockfd, msg, strlen(msg), 0);
     if(ret<0){
-        printf("send field");
+        printf("send field\r\n");
         close(sockfd);
         return;
     }
 }
 
 void epoll_prepare_fd(int sock) {
-    int opts = O_RDWR | O_NONBLOCK;
+    int opts = fcntl(sock, F_GETFL, 0);
+    opts |= O_NONBLOCK;
     if(fcntl(sock, F_SETFL, opts) < 0) {
-        printf("fcntl(sock, SETFL, opts)");
-        exit(1);
+        printf("set nonblocking error\r\n");
     }
 }
 
@@ -94,14 +94,22 @@ int epoll_add(int efd, int fd){
     struct epoll_event ev;
     ev.events = EPOLLIN|EPOLLET|EPOLLERR|EPOLLHUP;
     ev.data.fd = fd;
-    return epoll_ctl(efd, EPOLL_CTL_ADD, fd, &ev);
+    if(epoll_ctl(efd, EPOLL_CTL_ADD, fd, &ev)<0){
+        printf("epoll add error\r\n");
+        return -1;
+    }
+    return 0;
 }
 
 int epoll_set(int efd, int fd, int event_code){
     struct epoll_event ev;
     ev.events = event_code|EPOLLERR|EPOLLHUP;
     ev.data.fd = fd;
-    return epoll_ctl(efd, EPOLL_CTL_MOD, fd, &ev);
+    if(epoll_ctl(efd, EPOLL_CTL_MOD, fd, &ev)<0){
+        printf("epoll set error\r\n");
+        return -1;
+    }
+    return 0;
 }
 
 int epoll_del(int efd, int fd){
@@ -110,7 +118,11 @@ int epoll_del(int efd, int fd){
     struct epoll_event ev;
     ev.data.fd = fd;
     ev.events = -1;
-    return epoll_ctl(efd, EPOLL_CTL_DEL, fd, &ev);
+    if(epoll_ctl(efd, EPOLL_CTL_DEL, fd, &ev)<0){
+        printf("epoll del error\r\n");
+        return -1;
+    }
+    return 0;
 }
 
 int main(){
