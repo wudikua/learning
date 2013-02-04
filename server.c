9d9e252f2d6c9c905c360ad4074e38f7d0c1115d
jun meng <mengjun1990@gmail.com>
2013-02-04 13:58:32 +0800

    separate epll operate

--- server.c
+++ server.c
@@ -22,7 +22,6 @@ int create(){
 
 void bind2sock(int server_sockfd,int port){
     struct sockaddr_in server_addr;
- 
     bzero(&server_addr, sizeof(struct sockaddr_in));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = 
@@ -46,15 +45,28 @@ void listening(int server_sockfd){
 }
 
 void socket_recv(int sockfd){
-    char buffer[1024];
-    int recv_bytes = recv(sockfd, buffer, sizeof(buffer), 0);
-    if(recv_bytes<0){
-        printf("recv field");
-        close(sockfd);
-        return;
+    char buffer[512];
+    int flag = 1;
+    int recv_bytes = 0;
+    printf("\r\nrecv data\r\n");
+    while(flag){
+        recv_bytes = recv(sockfd, buffer, sizeof(buffer), 0);
+        if(recv_bytes<=0){
+            printf("recv field");
+            close(sockfd);
+            return;
+        }else{
+            buffer[recv_bytes] = '\0';
+            printf("%s", buffer);
+            bzero(&buffer,sizeof(buffer));
+        }
+        if(recv_bytes==sizeof(buffer)){
+            flag = 1;
+        }else{
+            flag = 0;
+        }
     }
-    buffer[recv_bytes] = '\0';
-    printf("recv data %d bytes\r\n%s", recv_bytes, buffer);
+    return;
 }
 
 void socket_send(int sockfd, const char* msg){
@@ -66,27 +78,47 @@ void socket_send(int sockfd, const char* msg){
     }
 }
 
-void setnonblocking(int sock) {
-    int opts;
-    opts = fcntl(sock, F_GETFL);
-    if(opts < 0) {
-	perror("fcntl(sock, GETFL)");
-	exit(1);
-    }
-    opts = opts | O_NONBLOCK;
+void epoll_prepare_fd(int sock) {
+    int opts = O_RDWR | O_NONBLOCK;
     if(fcntl(sock, F_SETFL, opts) < 0) {
-        perror("fcntl(sock, SETFL, opts)");
+        printf("fcntl(sock, SETFL, opts)");
         exit(1);
     }
+}
+
+int epoll_init(int size){
+    return epoll_create(size);
+}
+
+int epoll_add(int efd, int fd){
+    struct epoll_event ev;
+    ev.events = EPOLLIN|EPOLLET|EPOLLERR|EPOLLHUP;
+    ev.data.fd = fd;
+    return epoll_ctl(efd, EPOLL_CTL_ADD, fd, &ev);
+}
+
+int epoll_set(int efd, int fd, int event_code){
+    struct epoll_event ev;
+    ev.events = event_code|EPOLLERR|EPOLLHUP;
+    ev.data.fd = fd;
+    return epoll_ctl(efd, EPOLL_CTL_MOD, fd, &ev);
+}
+
+int epoll_del(int efd, int fd){
+    if(fd<0)
+        return -1;
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = -1;
+    return epoll_ctl(efd, EPOLL_CTL_DEL, fd, &ev);
 }
 
 int main(){
     int server_sockfd, client_sockfd;
     int ret,addr_len;
     struct sockaddr_in client_addr;
-    char buffer[1024];
-    struct epoll_event ev;
-    struct epoll_event events[256];
+    struct epoll_event events[MAX_CONNS];
+    char* responce_msg = "HTTP/1.1 200 OK\r\nContent-Type:text/html;charset=UTF-8\r\n\r\nhello world\r\n";
     //create
     server_sockfd = create();
  
@@ -96,40 +128,41 @@ int main(){
     //listen
     listening(server_sockfd); 
     
-    int epollfd = epoll_create(256);
-    ev.events = EPOLLIN|EPOLLET;
-    ev.data.fd = server_sockfd;
-    epoll_ctl(epollfd, EPOLL_CTL_ADD, server_sockfd, &ev);//register socket to epoll
+    int efd = epoll_init(MAX_CONNS);
+
+    epoll_prepare_fd(server_sockfd);
+    epoll_add(efd, server_sockfd);
     
     //accept loop
     while(1){
-        int nfds = epoll_wait(epollfd, events, 256, -1);
+        int nfds = epoll_wait(efd, events, MAX_CONNS, -1);
         printf("epoll triggered\r\n");
         int i=0;
-	for(i=0;i<nfds;i++){
+        for(i=0;i<nfds;i++){
             if(events[i].data.fd==server_sockfd){
                 addr_len = sizeof(struct sockaddr);
                 client_sockfd = accept(server_sockfd, (struct sockaddr*)(&client_addr), &addr_len);
-                printf("accept ok!\r\nServer start get connect from %s : %d\r\n",inet_ntoa(client_addr.sin_addr),client_addr.sin_port);
-                setnonblocking(client_sockfd); 
-				ev.data.fd = client_sockfd;
-                ev.events = EPOLLIN|EPOLLOUT|EPOLLET;
-                epoll_ctl(epollfd, EPOLL_CTL_ADD, client_sockfd, &ev);//register client socket to epoll
+                printf("accept ok!\r\nServer start get connect from %s : %d\r\n",(char*)inet_ntoa(client_addr.sin_addr),client_addr.sin_port);
+                epoll_prepare_fd(client_sockfd); 
+                epoll_add(efd, client_sockfd);
             }else if(events[i].events==EPOLLIN){
+                printf("epoll in \r\n");
                 client_sockfd = events[i].data.fd;
                 socket_recv(client_sockfd);
-                ev.data.fd = client_sockfd;
-				ev.events = EPOLLOUT|EPOLLET;
-				epoll_ctl(epollfd, EPOLL_CTL_MOD, client_sockfd, &ev);   
+                epoll_set(efd, client_sockfd, EPOLLOUT);
             }else if(events[i].events==EPOLLOUT){
+                printf("epoll out \r\n");
                 client_sockfd = events[i].data.fd;
-                char* msg = "HTTP/1.1 200 OK\r\nContent-Type:text/html;charset=UTF-8\r\n\r\nhello world\r\n";
-                socket_send(client_sockfd, msg);
-				ev.data.fd = client_sockfd;
-				epoll_ctl(epollfd, EPOLL_CTL_DEL, client_sockfd, &ev);
-				close(client_sockfd);
-			}
+                socket_send(client_sockfd, responce_msg);
+                epoll_del(efd, client_sockfd);
+                close(client_sockfd);
+            }else{
+                epoll_del(efd, events[i].data.fd);
+                close(events[i].data.fd);
+            }
         }
     }
+    close(efd);
+    free(events);
     return 0;
 }
